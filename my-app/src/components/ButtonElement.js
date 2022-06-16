5250644ed1aa03a0b1c50b7860e3c466bfb37166
Neel Soneji <neel8758@gmail.com>
2022-06-17 00:16:49 +0530

    last change

--- my-app/src/components/ButtonElement.js
+++ my-app/src/components/ButtonElement.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
 
-export const Button = styled(Link)
-`
+export const Button = styled(Link)`
   border-radius: 50px;
   background: ${({ primary }) => (primary ? "#01BF71" : "#010606")};
   white-space: nowrap;
